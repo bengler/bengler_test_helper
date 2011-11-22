dc7434e2692c44e85a0f5047ae0875b8ddb9ca6a
Alexander Staubo <alex@origo.no>
2011-11-22 16:24:02 +0100

    Require tasks automatically.

--- lib/bengler_test_helper/tasks.rb
+++ lib/bengler_test_helper/tasks.rb
@@ -1,3 +1,5 @@
+require 'rake'
+
 Rake::TaskManager.class_eval do
   unless method_defined?(:remove_task)
     def remove_task(task_name)
@@ -10,6 +12,7 @@ Rake.application.remove_task :"db:test:prepare"
 Rake.application.remove_task :"db:structure:dump"
 
 require 'bengler_test_helper/active_record'
+
 require 'bengler_test_helper/tasks/rails_db_test_prepare'
 require 'bengler_test_helper/tasks/rails_db_bootstrap_data'
 require 'bengler_test_helper/tasks/rails_db_load_structure'
