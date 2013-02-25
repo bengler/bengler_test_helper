52cc24feba422b743f0e49c58911a9c398314ec7
Erik Grinaker <erik@bengler.no>
2013-02-25 17:42:25 +0700

    Get project name from origin URI.

--- lib/bengler_test_helper/tasks/db_test_config.rb
+++ lib/bengler_test_helper/tasks/db_test_config.rb
@@ -1,8 +1,8 @@
 namespace :db do
   namespace :test do
     desc 'Sets up a database config file.'
-    task :config, :name do |t, args|
-      name = args[:name] || File.basename(Dir.pwd)
+    task :config do
+      name = File.basename(`git config --get remote.origin.url`.strip, '.git')
       File.write('config/database.yml', <<-end
 test:
   adapter: postgresql
