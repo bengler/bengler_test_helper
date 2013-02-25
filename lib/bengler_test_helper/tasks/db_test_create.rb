76606185f434bfae766e78fcabf4e1879ff382c1
Erik Grinaker <erik@bengler.no>
2013-02-25 16:45:22 +0700

    Fixed dropdb call.

--- lib/bengler_test_helper/tasks/db_test_create.rb
+++ lib/bengler_test_helper/tasks/db_test_create.rb
@@ -5,15 +5,15 @@ namespace :db do
       config = BenglerTestHelper::ActiveRecord.database_configuration('test')
       abort 'Database configuration must use localhost.' unless %w(localhost 127.0.0.1).include?(config['host'])
 
-      unless system("psql -ltA | grep -E '^#{config['database']}\\|' >/dev/null")
-        unless system("dropdb '#{config['database']}'")
-          abort "Could not drop database '#{config['database']}'"
-        end
-      end
-
       unless system("psql -tA postgres -c '\\du' | grep -E '^#{config['username']}\\|' >/dev/null")
         unless system("createuser -s '#{config['username']}'")
           abort "Could not create database user '#{config['username']}'"
+        end
+      end
+
+      if system("psql -ltA | grep -E '^#{config['database']}\\|' >/dev/null")
+        unless system("dropdb '#{config['database']}'")
+          abort "Could not drop database '#{config['database']}'"
         end
       end
 
