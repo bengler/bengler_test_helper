76e98fb83bc7f8504786ad090ae8409b17a779fc
Erik Grinaker <erik@bengler.no>
2013-03-05 21:31:45 +0700

    Fix property name.

--- lib/bengler_test_helper/active_record.rb
+++ lib/bengler_test_helper/active_record.rb
@@ -5,7 +5,7 @@ module BenglerTestHelper
       def database_configuration(environment)
         require 'active_record'
         ::ActiveRecord::Base.configurations = YAML.load(File.open('config/database.yml'))
-        config = ::ActiveRecord::Base.configuration[environment]
+        config = ::ActiveRecord::Base.configurations[environment]
         raise "No database configuration for environment #{environment.inspect}." unless config
         config
       end
