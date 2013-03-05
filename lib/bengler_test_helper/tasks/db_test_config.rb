4f8dae096ae939520e61825a0d88b0fc1c598828
Erik Grinaker <erik@bengler.no>
2013-03-05 17:04:18 +0700

    Ruby 1.8 fixes.

--- lib/bengler_test_helper/tasks/db_test_config.rb
+++ lib/bengler_test_helper/tasks/db_test_config.rb
@@ -3,7 +3,7 @@ namespace :db do
     desc 'Sets up a database config file.'
     task :config do
       name = File.basename(`git config --get remote.origin.url`.strip, '.git')
-      File.write('config/database.yml', <<-end
+      File.open('config/database.yml', 'w').write <<-end
 test:
   adapter: postgresql
   host: localhost
@@ -12,7 +12,6 @@ test:
   password: #{(0...8).map{(97+rand(26)).chr}.join}
   encoding: unicode
 end
-      )
     end
   end
 end
