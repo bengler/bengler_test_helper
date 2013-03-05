5198c11e2ae4b61dd161343e363afc5352931294
Erik Grinaker <erik@bengler.no>
2013-03-05 21:17:49 +0700

    Workaround for ActiveRecord database configuration bug.

--- lib/bengler_test_helper/active_record.rb
+++ lib/bengler_test_helper/active_record.rb
@@ -5,10 +5,10 @@ module BenglerTestHelper
       def database_configuration(environment)
         require 'active_record'
         config = YAML.load(File.open('config/database.yml'))
+        envconfig = config[environment] and config[environment].clone
         ::ActiveRecord::Base.configurations = config
-        config = config[environment] # can't fetch from ActiveRecord due to bug
-        raise "No database configuration for environment #{environment.inspect}." unless config
-        config
+        raise "No database configuration for environment #{environment.inspect}." unless envconfig
+        envconfig
       end
     end
 
