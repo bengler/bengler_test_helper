625e7880b5182b27bfafaf9ec7ef1590e5daf218
Simen Svale Skogsrud <simen@bengler.no>
2013-03-21 11:03:24 +0100

    Doh

--- lib/bengler_test_helper/tasks/db_migrate.rb
+++ lib/bengler_test_helper/tasks/db_migrate.rb
@@ -1,5 +1,5 @@
 namespace :db do
   Rake::Task['migrate'].enhance do
-    Rake::Task['db:structure:dump'].invoke unless ENV['RACK_ENV'] == 'development'
+    Rake::Task['db:structure:dump'].invoke if ENV['RACK_ENV'] == 'development'
   end
 end
