ea22e1ecc14d77fdc47c1f9485accccee6061b0b
Alexander Staubo <alex@purefiction.net>
2013-09-01 00:43:49 -0400

    Fix db:structure:load to run script as database user, otherwise owner will become the current user.

--- lib/bengler_test_helper/tasks/db_structure_load.rb
+++ lib/bengler_test_helper/tasks/db_structure_load.rb
@@ -6,7 +6,7 @@ namespace :db do
       abort 'Database configuration must use localhost.' unless %w(localhost 127.0.0.1).include?(config['host'])
 
       schema_file_name = 'db/development_structure.sql'
-      unless system("psql -1 -q -o /dev/null --no-psqlrc -d '#{config['database']}' -f '#{schema_file_name}'")
+      unless system("psql -U #{config['username']} -1 -q -o /dev/null --no-psqlrc -d '#{config['database']}' -f '#{schema_file_name}'")
         abort "Failed to load SQL from #{schema_file_name}. Ensure that your own Postgres user is a superuser."
       end
 
